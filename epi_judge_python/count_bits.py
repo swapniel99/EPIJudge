93bf11a58d0c1acd506a1a20f9a0f013c21a1d07
Swapnil Gusani <swapniel99@gmail.com>
2021-02-19 03:02:58 +0530

    parity

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -2,11 +2,12 @@ from test_framework import generic_test
 
 
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_bits = 0
+    while x:
+        num_bits += 1
+        x &= x - 1
+    return num_bits
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
-                                       count_bits))
+    exit(generic_test.generic_test_main('count_bits.py', 'count_bits.tsv', count_bits))
--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,53 @@
 from test_framework import generic_test
 
+# n is number of bits.
 
-def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+# # O(n, 1)
+# def parity(x: int) -> int:
+#     res = 0
+#     while x:
+#         res ^= x & 1
+#         x >>= 1
+#     return res
+
+
+# O(n, 1) in worst case
+# def parity(x: int) -> int:
+#     res = 0
+#     while x:
+#         res ^= 1
+#         x = x & (x - 1)
+#     return res
+
+# # This is a costly operation.
+# def bitlength(x):
+#     from math import log2, ceil
+#     return 1 << ceil(log2(len(bin(x)) - 2))
+#
+#
+# # O(log n, 1)
+# def parity(x: int) -> int:
+#     if x <= 0xFFFFFFFFFFFFFFFF:
+#         bits = 64
+#     else:
+#         bits = bitlength(x)
+#     while bits > 1:
+#         bits //= 2
+#         x ^= x >> bits
+#     return x & 1
+
+
+# O((n//64) + log(64))
+def parity(n):
+    x = 0
+    bits = 64
+    while n:
+        x ^= n & 0xFFFFFFFFFFFFFFFF
+        n >>= bits
+    while bits > 1:
+        bits //= 2
+        x ^= x >> bits
+    return x & 0x1
 
 
 if __name__ == '__main__':
