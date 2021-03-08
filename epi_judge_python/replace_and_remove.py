87e6ee4abeba439b354e51d25722228effd49b93
Swapnil Gusani <swapniel99@gmail.com>
2021-03-08 18:14:43 +0530

    replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -6,8 +6,32 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    ca = 0
+    cb = 0
+    for c in s:
+        if c == 'a':
+            ca += 1
+        elif c == 'b':
+            cb += 1
+    final_len = size + ca - cb
+
+    reader = size - 1
+    writer = -1
+
+    while reader >= 0:
+        if s[reader] == 'a':
+            s[writer] = 'd'
+            s[writer-1] = 'd'
+            writer -= 2
+        elif s[reader] != 'b':
+            s[writer] = s[reader]
+            writer -= 1
+        reader -= 1
+
+    for i in range(final_len):
+        s[i] = s[-final_len + i]
+
+    return final_len
 
 
 @enable_executor_hook
@@ -17,7 +41,4 @@ def replace_and_remove_wrapper(executor, size, s):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('replace_and_remove.py',
-                                       'replace_and_remove.tsv',
-                                       replace_and_remove_wrapper))
+    exit(generic_test.generic_test_main('replace_and_remove.py', 'replace_and_remove.tsv', replace_and_remove_wrapper))
