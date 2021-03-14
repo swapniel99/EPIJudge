332e3f061b4efcb7dc63f4b3fc8ce529136ba1f8
Swapnil Gusani <swapniel99@gmail.com>
2021-03-14 18:40:01 +0530

    optimise

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -3,24 +3,18 @@ from test_framework import generic_test
 
 
 def reverse_sublist(L: ListNode, start: int, finish: int) -> ListNode:
-    if start == finish:
-        return L
-
     dummy = ListNode(0, L)
 
     prev = dummy
     for _ in range(start - 1):
         prev = prev.next
 
-    ptr0 = prev
-    ptr1 = ptr0.next
-    for _ in range(start, finish + 1):
-        temp = ptr1.next
-        ptr1.next = ptr0
-        ptr0, ptr1 = ptr1, temp
-
-    prev.next.next = ptr1
-    prev.next = ptr0
+    ptr = prev.next
+    for _ in range(start, finish):
+        temp = ptr.next
+        ptr.next = temp.next
+        temp.next = prev.next
+        prev.next = temp
 
     return dummy.next
 
