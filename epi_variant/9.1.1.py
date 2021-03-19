530b88fc4cc3cd640d7897a9035e7248e09e069b
Swapnil Gusani <swapniel99@gmail.com>
2021-03-20 02:22:05 +0530

    variant

---
+++ epi_variant/9.1.1.py
@@ -0,0 +1,24 @@
+from binary_tree_node import BinaryTreeNode
+
+
+def largest_complete_helper(node: BinaryTreeNode):
+    if node is None:
+        return True, True, 0, 0, 0
+
+    perleft, compleft, htleft, szleft, resleft = largest_complete_helper(node.left)
+    perright, compright, htright, szright, resright = largest_complete_helper(node.right)
+
+    perfect = perleft and perright and htleft == htright
+    complete = perfect \
+        or (perleft and compright and htleft == htright) \
+        or (perleft and perright and htleft == htright + 1) \
+        or (compleft and perright and htleft == htright + 1)
+    height = max(htleft, htright) + 1
+    size = szleft + szright + 1
+    res = size if complete else max(resleft, resright)
+
+    return perfect, complete, height, size, res
+
+
+def isCompleteTree(root: BinaryTreeNode) -> int:
+    return largest_complete_helper(root)[-1]
