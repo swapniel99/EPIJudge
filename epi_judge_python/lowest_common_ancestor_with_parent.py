98eb36c245069421bc8e55c7eebb318d2528168b
Swapnil Gusani <swapniel99@gmail.com>
2021-03-26 01:14:39 +0530

    lca with parent

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -8,17 +8,44 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def lca(node0: BinaryTreeNode,
-        node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+def lca(node0: BinaryTreeNode, node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+    ptr0 = node0
+    ptr1 = node1
+
+    while ptr0 is not ptr1:
+        ptr0 = ptr0.parent
+        ptr1 = ptr1.parent
+        if ptr0 is None or ptr1 is None:
+            break
+    else:
+        return ptr0
+
+    if ptr1 is None:
+        node0, node1 = node1, node0
+        ptr0, ptr1 = ptr1, ptr0
+
+    d = 0
+    while ptr1 is not None:
+        ptr1 = ptr1.parent
+        d += 1
+
+    ptr0 = node0
+    ptr1 = node1
+
+    for i in range(d):
+        ptr1 = ptr1.parent
+
+    while ptr0 is not ptr1:
+        ptr0 = ptr0.parent
+        ptr1 = ptr1.parent
+    else:
+        return ptr0
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, node0, node1):
     result = executor.run(
-        functools.partial(lca, must_find_node(tree, node0),
-                          must_find_node(tree, node1)))
+        functools.partial(lca, must_find_node(tree, node0), must_find_node(tree, node1)))
 
     if result is None:
         raise TestFailure('Result can\'t be None')
@@ -26,7 +53,5 @@ def lca_wrapper(executor, tree, node0, node1):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('lowest_common_ancestor_with_parent.py',
-                                       'lowest_common_ancestor.tsv',
-                                       lca_wrapper))
+    exit(generic_test.generic_test_main('lowest_common_ancestor_with_parent.py', 'lowest_common_ancestor.tsv',
+                                        lca_wrapper))
---
+++ epi_variant/9.2.2.py
@@ -0,0 +1,19 @@
+from binary_tree_node import BinaryTreeNode
+
+
+def k_balanced_binary_tree_helper(node: BinaryTreeNode, k: int):
+    if node is None:
+        return True, 0, None
+
+    leftb, leftn, leftres = k_balanced_binary_tree_helper(node.left, k)
+    rightb, rightn, rightres = k_balanced_binary_tree_helper(node.right, k)
+
+    balanced = abs(leftn - rightn) <= k
+    nodes = leftn + rightn + 1
+    res = node if not balanced and leftb and rightb else leftres or rightres
+
+    return balanced, nodes, res
+
+
+def is_k_balanced_binary_tree(tree: BinaryTreeNode, k: int) -> bool:
+    return k_balanced_binary_tree_helper(tree, k)[-1]
