20e7eb66061bdb22fb323de4854d4577c01e5aee
Swapnil Gusani <swapniel99@gmail.com>
2021-04-11 20:13:01 +0530

    hash 12.2

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,15 +1,14 @@
 from test_framework import generic_test
 
+from collections import Counter
 
-def is_letter_constructible_from_magazine(letter_text: str,
-                                          magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+
+def is_letter_constructible_from_magazine(letter_text: str, magazine_text: str) -> bool:
+    letter_count = Counter(letter_text)
+    magazine_count = Counter(magazine_text)
+    return all((ct <= magazine_count[char] for char, ct in letter_count.items()))
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            'is_anonymous_letter_constructible.py',
-            'is_anonymous_letter_constructible.tsv',
-            is_letter_constructible_from_magazine))
+    exit(generic_test.generic_test_main('is_anonymous_letter_constructible.py', 'is_anonymous_letter_constructible.tsv',
+                                        is_letter_constructible_from_magazine))
--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,12 +1,13 @@
 from test_framework import generic_test
 
-from collections import Counter
-
 
 def can_form_palindrome(s: str) -> bool:
-    return sum([v % 2 for v in Counter(s).values()]) <= 1
+    # TODO - you fill in here.
+    return True
 
 
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main('is_string_permutable_to_palindrome.py',
-                                        'is_string_permutable_to_palindrome.tsv', can_form_palindrome))
+    exit(
+        generic_test.generic_test_main('is_string_permutable_to_palindrome.py',
+                                       'is_string_permutable_to_palindrome.tsv',
+                                       can_form_palindrome))
