ff96966f218342f5575195ebdeae85dd6d7ca9b9
Swapnil Gusani <swapniel99@gmail.com>
2021-04-24 02:18:45 +0530

    strings

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,12 +2,22 @@ from test_framework import generic_test
 
 
 def is_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        if not s[i].isalnum():
+            i += 1
+        elif not s[j].isalnum():
+            j -= 1
+        elif s[i].lower() != s[j].lower():
+            return False
+        else:
+            i += 1
+            j -= 1
     return True
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            'is_string_palindromic_punctuation.py',
-            'is_string_palindromic_punctuation.tsv', is_palindrome))
+    exit(generic_test.generic_test_main('is_string_palindromic_punctuation.py', 'is_string_palindromic_punctuation.tsv',
+                                        is_palindrome))
--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -4,11 +4,26 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+def reverse_range(s, i, j):
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+
+
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
+    s.reverse()
+
+    i = j = 0
+    while j < len(s):
+        if s[j] == ' ':
+            reverse_range(s, i, j - 1)
+            i = j = j + 1
+        else:
+            j += 1
+    reverse_range(s, i, j - 1)
 
 
 @enable_executor_hook
@@ -20,7 +35,14 @@ def reverse_words_wrapper(executor, s):
     return ''.join(s_copy)
 
 
+def reverse_words2(s):
+    return ' '.join(reversed(s.split(' ')))
+
+
+@enable_executor_hook
+def reverse_words_wrapper2(executor, s):
+    return executor.run(functools.partial(reverse_words2, s))
+
+
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('reverse_words.py', 'reverse_words.tsv',
-                                       reverse_words_wrapper))
+    exit(generic_test.generic_test_main('reverse_words.py', 'reverse_words.tsv', reverse_words_wrapper))
