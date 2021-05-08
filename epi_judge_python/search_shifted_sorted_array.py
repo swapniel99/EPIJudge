764ecd27d4069f90f9aee1e32956eb30ea5f305e
Swapnil Gusani <swapniel99@gmail.com>
2021-05-08 16:08:16 +0530

    search shifted array

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -4,12 +4,21 @@ from test_framework import generic_test
 
 
 def search_smallest(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    start = 0
+    end = len(A) - 1
+
+    while start < end:
+        if A[start] < A[end]:
+            return start
+        mid = start + (end - start) // 2
+        if A[mid] < A[start]:
+            end = mid
+        else:
+            start = mid + 1
+
+    return start
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('search_shifted_sorted_array.py',
-                                       'search_shifted_sorted_array.tsv',
-                                       search_smallest))
+    exit(generic_test.generic_test_main('search_shifted_sorted_array.py', 'search_shifted_sorted_array.tsv',
+                                        search_smallest))
--- problem_mapping.js
+++ problem_mapping.js
@@ -1250,7 +1250,7 @@ problem_mapping = {
                 "total": 101
             },
             "Python: sort_almost_sorted_array.py": {
-                "passed": 0,
+                "passed": 101,
                 "total": 101
             }
         },
@@ -1336,7 +1336,7 @@ problem_mapping = {
                 "total": 307
             },
             "Python: search_shifted_sorted_array.py": {
-                "passed": 0,
+                "passed": 307,
                 "total": 307
             }
         },
